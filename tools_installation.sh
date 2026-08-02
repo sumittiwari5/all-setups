b4016afbf768cbcd7eeda813003209b63ac0a2eb
Sumit Tiwari <134449481+sumittiwari5@users.noreply.github.com>
2026-08-02 22:58:09 +0530

    Add installation script for Git, Docker, Jenkins, AWS CLI, Terraform, and Helm
    This script installs Git, Docker, Jenkins, AWS CLI, Terraform, and Helm on a system. It includes commands to set up necessary repositories and install each tool with appropriate dependencies.

---
+++ tools_installation.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# git installation
+sudo apt install git -y # use "yum" instead of "apt" for amazon linux
+
+#Install Docker
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+
+#Install Jenkins
+
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre -y
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
+
+
+#Install AWS CLI
+
+sudo apt install unzip -y 
+curl -fsSL https://awscli.amazonaws.com/v2/install.sh | bash
+
+curl -fsSL https://awscli.amazonaws.com/v2/install.sh | sudo bash -s -- --system
+
+curl -fsSL https://awscli.amazonaws.com/v2/install.sh | bash -s -- --help
+
+
+#Install Terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+
+sudo apt-get install terraform -y
+
+
+#Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
+
